0e2cae7e56b4eb33f6319d449c487bf872729367
Will Salt <williamejsalt@gmail.com>
2017-01-24 13:09:58 +0000

    Create DNS Zone and assosiate elb with www (#4)
    * Create DNS zone for willejs.io and create record
    
    * add comments to route53 records and zones

---
+++ providers/aws/route53.tf
@@ -0,0 +1,21 @@
+// create zone for willejs.io domain
+resource "aws_route53_zone" "willejs_io" {
+  name = "willejs.io"
+
+  tags {
+    Name = "willejs.io"
+  }
+}
+
+// Create just the www.willejs.io record
+resource "aws_route53_record" "www_willejs_io" {
+  zone_id = "${aws_route53_zone.willejs_io.zone_id}"
+  name = "www.willejs.io"
+  type = "CNAME"
+  ttl = "30"
+  records = [
+    "${module.ecs_cluster_1.elb_dns_name}"
+  ]
+}
+
+// TODO: Create record for S3 bucket that redirects A record to WWW.
