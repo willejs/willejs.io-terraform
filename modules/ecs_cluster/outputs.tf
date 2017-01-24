6edfe29b58231672b4371fb567c86d9252dc2dba
Will Salt <williamejsalt@gmail.com>
2017-01-24 13:27:58 +0000

    Add redirect from root domain to www. subdomain with s3 bucket

--- modules/ecs_cluster/outputs.tf
+++ modules/ecs_cluster/outputs.tf
@@ -7,3 +7,11 @@ output "elb_dns_name" {
 
   // description = "Output the dns name of the load balancer for DNS records"
 }
+
+output "elb_zone_id" {
+  value = "${aws_elb.ecs_asg_elb.zone_id}"
+
+  // outputs dont support descriptions yet!
+
+  // description = "Output the dns zone of the load balancer for DNS alias record"
+}
--- providers/aws/route53.tf
+++ providers/aws/route53.tf
@@ -19,5 +19,32 @@ resource "aws_route53_record" "www_willejs_io" {
   ]
 }
 
-// TODO: Create record for S3 bucket that redirects A record to WWW.
+// Set up the root A record, obviously this has to be done via an alias.
+resource "aws_route53_record" "redirect_willejs_io" {
+  zone_id = "${aws_route53_zone.willejs_io.zone_id}"
+  name    = "willejs.io"
+  type    = "A"
 
+  alias {
+    name                   = "${aws_s3_bucket.redirect_willejs_io.website_domain}"
+    zone_id                = "${aws_s3_bucket.redirect_willejs_io.hosted_zone_id}"
+    evaluate_target_health = false
+  }
+}
+
+// Set up an empty bucket that just does a redirect
+resource "aws_s3_bucket" "redirect_willejs_io" {
+  bucket = "willejs.io"
+  acl    = "public-read"
+
+  website {
+    redirect_all_requests_to = "www.willejs.io"
+  }
+
+  tags {
+    Name        = "willejs.io"
+    environment = "${var.environment}"
+    project     = "${var.project_name}"
+    description = "Redirect to the willejs_io ELB"
+  }
+}
